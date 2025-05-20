6e91250ebe04caa39723d0b1ed65818466492b09
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-05-20 14:03:59 +0200

    [UPD] Milestone 1 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,45 @@
+import axios from "axios"
+import { useState, useEffect } from "react"
+
+const API = 'https://api.themoviedb.org/3/search/movie?api_key=d732698a8c274162c8f3494383cd5a67&language=it-IT&query='
+
 function App() {
+
+  const [movies, setMovies] = useState([])
+  const [search, setSearch] = useState('')
+
+  const getMovies = () => {
+    axios.get(`${API}${search}`)
+      .then(res => {
+        setMovies(res.data.results)
+      })
+  }
+
+  const searchMovies = (e) => {
+    setSearch(e.target.value)
+  }
 
   return (
     <>
-
+      <header>
+        <input type="text" onChange={(e) => searchMovies(e)} value={search}></input>
+        <button onClick={() => getMovies()}>cerca</button>
+      </header>
+      <main>
+        <ul>
+          {movies.map((movie, index) => (
+            <li key={movie.id}>
+              elemento {index + 1}
+              <ul>
+                <li>{movie.title}</li>
+                <li>{movie.original_title}</li>
+                <li>{movie.vote_average}</li>
+                <li>{movie.original_language}</li>
+              </ul>
+            </li>
+          ))}
+        </ul>
+      </main>
     </>
   )
 }
